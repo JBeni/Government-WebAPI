b2b29779be678837bab3a8cb7937a6a8f4e7d684
JBeni <jitca.beniamin@gmail.com>
2021-03-17 10:45:21 +0200

    create model for response

---
+++ Application/Common/Models/RequestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GovernmentSystem.Application.Common.Models
+{
+    public class RequestResponse
+    {
+        public bool Successful { get; set; }
+        public Exception Exception { get; set; }
+
+
+		public static RequestResponse Success()
+		{
+			return new RequestResponse { Successful = true, Exception = null };
+		}
+
+		public static RequestResponse Failure(Exception exception)
+		{
+			return new RequestResponse { Successful = false, Exception = exception };
+		}
+	}
+}
--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -16,9 +16,7 @@ namespace GovernmentSystem.Application
             services.AddMediatR(Assembly.GetExecutingAssembly());
 
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehaviour<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
-            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
 
             return services;
         }
