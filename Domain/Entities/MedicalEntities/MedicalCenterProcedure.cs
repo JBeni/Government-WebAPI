48c639427f049fc2617cc3904fe44804110f984f
JBeni <jitca.beniamin@gmail.com>
2021-03-22 22:14:51 +0200

    connection table for MedicalCenter and MedicalProcedure

---
+++ Domain/Entities/MedicalEntities/MedicalCenterProcedure.cs
@@ -0,0 +1,10 @@
+﻿using GovernmentSystem.Domain.Common;
+
+namespace GovernmentSystem.Domain.Entities.MedicalEntities
+{
+    public class MedicalCenterProcedure : BaseEntity
+    {
+        public MedicalCenter MedicalCenter { get; set; }
+        public MedicalProcedure MedicalProcedure { get; set; }
+    }
+}
