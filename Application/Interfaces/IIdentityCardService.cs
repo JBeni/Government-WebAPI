59ef7ab82fda231d6ec9706e23672711ca5cddd5
JBeni <jitca.beniamin@gmail.com>
2021-03-24 11:04:59 +0200

    create handlers and service for Identity Card Entity

---
+++ Application/Interfaces/IIdentityCardService.cs
@@ -0,0 +1,18 @@
+﻿using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.IdentityCards.Commands;
+using GovernmentSystem.Application.Handlers.IdentityCards.Queries;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface IIdentityCardService
+    {
+        Task<RequestResponse> CreateIdentityCard(CreateIdentityCardCommand command, CancellationToken cancellationToken);
+        Task<RequestResponse> DeleteIdentityCard(DeleteIdentityCardCommand command, CancellationToken cancellationToken);
+        IdentityCardResponse GetIdentityCardById(GetIdentityCardByIdQuery query);
+        List<IdentityCardsResponse> GetIdentityCards(GetIdentityCardsQuery query);
+        Task<RequestResponse> UpdateIdentityCard(UpdateIdentityCardCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ Infrastructure/Services/IdentityCardService.cs
@@ -0,0 +1,39 @@
+﻿using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.IdentityCards.Commands;
+using GovernmentSystem.Application.Handlers.IdentityCards.Queries;
+using GovernmentSystem.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Infrastructure.Services
+{
+    public class IdentityCardService : IIdentityCardService
+    {
+        public Task<RequestResponse> CreateIdentityCard(CreateIdentityCardCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RequestResponse> DeleteIdentityCard(DeleteIdentityCardCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IdentityCardResponse GetIdentityCardById(GetIdentityCardByIdQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IdentityCardsResponse> GetIdentityCards(GetIdentityCardsQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<RequestResponse> UpdateIdentityCard(UpdateIdentityCardCommand command, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
