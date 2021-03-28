8ca7dae53e6382aadd6947f4afdadf0535d3ec07
JBeni <jitca.beniamin@gmail.com>
2021-03-28 21:16:50 +0300

    added error message for medical center procedure queries service

--- Application/Handlers/MedicalCenterProcedures/Queries/GetMedicalCenterProcedureByIdQuery.cs
+++ Application/Handlers/MedicalCenterProcedures/Queries/GetMedicalCenterProcedureByIdQuery.cs
@@ -30,7 +30,7 @@ namespace GovernmentSystem.Application.Handlers.MedicalCenterProcedures.Queries
             }
             catch (Exception ex)
             {
-                throw new Exception("There was an error retrieving the public servants of serious fraud office", ex);
+                throw new Exception("There was an error retrieving the medical center procedure by Id", ex);
             }
         }
     }
