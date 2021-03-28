222e5c64d960286c42016274712b57aaffd1bcd4
JBeni <jitca.beniamin@gmail.com>
2021-03-28 21:13:40 +0300

    added error message for birth certificate queries service

--- Application/Handlers/BirthCertificates/Queries/GetBirthCertificateByIdQuery.cs
+++ Application/Handlers/BirthCertificates/Queries/GetBirthCertificateByIdQuery.cs
@@ -30,7 +30,7 @@ namespace GovernmentSystem.Application.Handlers.BirthCertificates.Queries
             }
             catch (Exception ex)
             {
-                throw new Exception("", ex);
+                throw new Exception("There was an error retrieving the birth certificate by Id", ex);
             }
         }
     }
