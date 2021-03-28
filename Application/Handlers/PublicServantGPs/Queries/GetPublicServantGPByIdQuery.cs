be2bb68c42903c2c0e22e4cccb2db3b2acd4b86e
JBeni <jitca.beniamin@gmail.com>
2021-03-28 21:19:43 +0300

    added error message for public servant GP queries service

--- Application/Handlers/PublicServantGPs/Queries/GetPublicServantGPByIdQuery.cs
+++ Application/Handlers/PublicServantGPs/Queries/GetPublicServantGPByIdQuery.cs
@@ -30,7 +30,7 @@ namespace GovernmentSystem.Application.Handlers.PublicServantGPs.Queries
             }
             catch (Exception ex)
             {
-                throw new Exception("There was an error retrieving the public servants of serious fraud office", ex);
+                throw new Exception("There was an error retrieving the public servant of GP by Id", ex);
             }
         }
     }
