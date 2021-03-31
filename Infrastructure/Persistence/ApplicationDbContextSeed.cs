eb3b83e43bf748a770d25fbf370939e1812d7ccb
JBeni <jitca.beniamin@gmail.com>
2021-03-31 19:03:52 +0300

    seed data for DriverLicenseCategory Entity

--- Infrastructure/Persistence/ApplicationDbContextSeed.cs
+++ Infrastructure/Persistence/ApplicationDbContextSeed.cs
@@ -28,6 +28,30 @@ namespace GovernmentSystem.Infrastructure.Persistence
         //    }
         //}
 
+        public static async Task SeedSampleDriverLicenseCategoryAsync(ApplicationDbContext context)
+        {
+            if (!context.DriverLicenseCategories.Any())
+            {
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "None" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "Am" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "A1" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "A2" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "A" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "B1" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "B" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "C1" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "C" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "D1" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "D" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "BE" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "C1E" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "CE" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "D1E" });
+                context.DriverLicenseCategories.Add(new DriverLicenseCategory { Category = "DE" });
+                await context.SaveChangesAsync();
+            }
+        }
+
         public static async Task SeedSampleDataAsync(ApplicationDbContext context)
         {
             if (!context.Citizens.Any())
@@ -38,8 +62,6 @@ namespace GovernmentSystem.Infrastructure.Persistence
                         FirstName = "Beni",
                         LastName = "Jitca",
                         CNP = "1933423129812",
-                        //Passport = "sdfsdfs",
-                        //CI = "dsfsdf",
                         Age = 21,
                         DateOfBirth = new DateTime(1990, 9, 25)
                     }
