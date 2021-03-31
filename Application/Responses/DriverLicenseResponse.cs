b583d06d1fa88c0b30426cb252d433be24789130
JBeni <jitca.beniamin@gmail.com>
2021-03-31 18:58:11 +0300

    update DriverLicneseResponse model

--- Application/Responses/DriverLicenseResponse.cs
+++ Application/Responses/DriverLicenseResponse.cs
@@ -9,14 +9,12 @@ namespace GovernmentSystem.Application.Responses
     {
         public Guid Identifier { get; set; }
         public string LicenseNumber { get; set; }
-        public DriverLicenseCategory Category { get; set; }
 
         public void Mapping(Profile profile)
         {
             profile.CreateMap<DriverLicense, DriverLicenseResponse>()
                 .ForMember(d => d.Identifier, opt => opt.MapFrom(s => s.Identifier))
-                .ForMember(d => d.LicenseNumber, opt => opt.MapFrom(s => s.LicenseNumber))
-                .ForMember(d => d.Category, opt => opt.MapFrom(s => s.Category));
+                .ForMember(d => d.LicenseNumber, opt => opt.MapFrom(s => s.LicenseNumber));
         }
     }
 }
