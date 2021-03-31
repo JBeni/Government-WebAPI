a88d505dab4aa46f642d1303d3aa5ee59449bac6
JBeni <jitca.beniamin@gmail.com>
2021-03-31 19:02:54 +0300

    create CitizenDriverLicenseCategoryService

---
+++ Application/Interfaces/ICitizenDriverLicenseCategoryService.cs
@@ -0,0 +1,12 @@
+﻿using GovernmentSystem.Application.Handlers.CitizenDriverLicenseCategories.Queries;
+using GovernmentSystem.Application.Responses;
+using System.Collections.Generic;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface ICitizenDriverLicenseCategoryService
+    {
+        CitizenDriverLicenseCategoryResponse GetCitizenDriverLicenseCategoryById(GetCitizenDriverLicenseCategoryByIdQuery query);
+        List<CitizenDriverLicenseCategoryResponse> GetCitizenDriverLicenseCategories(GetCitizenDriverLicenseCategoriesQuery query);
+    }
+}
