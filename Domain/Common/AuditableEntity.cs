2e1dfd51e02df0707024345bf8850d8681228ee4
JBeni <jitca.beniamin@gmail.com>
2021-04-01 23:06:47 +0300

    remove IsDeleted member for AuditableEntity

--- Domain/Common/AuditableEntity.cs
+++ Domain/Common/AuditableEntity.cs
@@ -4,7 +4,6 @@ namespace GovernmentSystem.Domain.Common
 {
     public abstract class AuditableEntity : BaseEntity
     {
-        public bool IsDeleted { get; set; }
         public DateTime AuditEntryCreated { get; set; }
         public string AuditEntryCreatedBy { get; set; }
         public DateTime? AuditEntryLastModified { get; set; }
