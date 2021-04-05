f313e5ea774aaeb833808aee15c2c4dce107187e
JBeni <jitca.beniamin@gmail.com>
2021-04-05 23:03:53 +0300

    create new tables for Serious Fraud Office services to be used by the citizen

---
+++ Domain/Entities/SeriousFraudOffices/Company.cs
@@ -0,0 +1,8 @@
+﻿using GovernmentSystem.Domain.Common;
+
+namespace GovernmentSystem.Domain.Entities.SeriousFraudOffices
+{
+    public class Company : AuditEntity
+    {
+    }
+}
---
+++ Domain/Entities/SeriousFraudOffices/FraudOfficeReportProblem.cs
@@ -0,0 +1,9 @@
+﻿using GovernmentSystem.Domain.Common;
+
+namespace GovernmentSystem.Domain.Entities.SeriousFraudOffices
+{
+    public class FraudOfficeReportProblem : ReportProblem
+    {
+        public SeriousFraudOffice SeriousFraudOffice { get; set; }
+    }
+}
