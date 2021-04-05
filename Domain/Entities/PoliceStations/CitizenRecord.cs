d843757151ca39b6fe3c2cbfa27e439f73bfe7b3
JBeni <jitca.beniamin@gmail.com>
2021-04-05 22:36:05 +0300

    create new tables for Police services to be used by the citizen

---
+++ Domain/Entities/PoliceStations/CitizenRecord.cs
@@ -0,0 +1,8 @@
+﻿using GovernmentSystem.Domain.Common;
+
+namespace GovernmentSystem.Domain.Entities.PoliceStations
+{
+    public class CitizenRecord : AuditEntity
+    {
+    }
+}
---
+++ Domain/Entities/PoliceStations/PoliceReportProblem.cs
@@ -0,0 +1,9 @@
+﻿using GovernmentSystem.Domain.Common;
+
+namespace GovernmentSystem.Domain.Entities.PoliceStations
+{
+    public class PoliceReportProblem : ReportProblem
+    {
+        public PoliceStation PoliceStation { get; set; }
+    }
+}
