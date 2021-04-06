0360fb396635bdd4bb6d3dad7d6456e9e2e19ae3
JBeni <jitca.beniamin@gmail.com>
2021-04-06 16:04:07 +0300

    create configuration for IdentityCardAppointment

---
+++ Infrastructure/Persistence/Configurations/IdentityCardAppointmentConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using GovernmentSystem.Domain.Entities.PoliceStations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class IdentityCardAppointmentConfiguration : IEntityTypeConfiguration<IdentityCardAppointment>
+    {
+        public void Configure(EntityTypeBuilder<IdentityCardAppointment> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.Reason)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.AppointmentDay)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.AdditionalInformation)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
