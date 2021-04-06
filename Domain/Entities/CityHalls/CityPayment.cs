0124f163d4bdc1fc6bb7cdb14a5a98b7dc239cd6
JBeni <jitca.beniamin@gmail.com>
2021-04-06 18:27:43 +0300

    change name of CityPayment entity

--- Domain/Entities/CityHalls/CityPayment.cs
+++ Domain/Entities/CityHalls/CityPayment.cs
@@ -3,7 +3,7 @@ using GovernmentSystem.Domain.Entities.Citizens;
 
 namespace GovernmentSystem.Domain.Entities.CityHalls
 {
-    public class CityHallPayment : Payment
+    public class CityPayment : Payment
     {
         public Invoice Invoice { get; set; }
         public CityHall CityHall { get; set; }
