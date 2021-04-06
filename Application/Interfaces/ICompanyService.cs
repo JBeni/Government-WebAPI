6fad19c248e539616d7ef55e1fe7b66bd0fb333a
JBeni <jitca.beniamin@gmail.com>
2021-04-06 22:21:43 +0300

    create Company service

---
+++ Application/Interfaces/ICompanyService.cs
@@ -0,0 +1,19 @@
+﻿using GovernmentSystem.Application.Handlers.Companies.Commands;
+using GovernmentSystem.Application.Common.Models;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using GovernmentSystem.Application.Handlers.Companies.Queries;
+using GovernmentSystem.Application.Responses;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface ICompanyService
+    {
+        Task<RequestResponse> CreateCompany(CreateCompanyCommand command, CancellationToken cancellationToken);
+        Task<RequestResponse> DeleteCompany(DeleteCompanyCommand command, CancellationToken cancellationToken);
+        CompanyResponse GetCompanyById(GetCompanyByIdQuery query);
+        List<CompanyResponse> GetCompanies(GetCompaniesQuery query);
+        Task<RequestResponse> UpdateCompany(UpdateCompanyCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ Infrastructure/Services/CompanyService.cs
@@ -0,0 +1,114 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GovernmentSystem.Application.Common.Interfaces;
+using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.Companies.Commands;
+using GovernmentSystem.Application.Handlers.Companies.Queries;
+using GovernmentSystem.Application.Interfaces;
+using GovernmentSystem.Application.Responses;
+using GovernmentSystem.Domain.Entities.SeriousFraudOffices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Infrastructure.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly IInsideEntityService _insideEntityService;
+
+        public CompanyService(IApplicationDbContext dbContext, IMapper mapper, IInsideEntityService insideEntityService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _insideEntityService = insideEntityService;
+        }
+
+        public async Task<RequestResponse> CreateCompany(CreateCompanyCommand command, CancellationToken cancellationToken)
+        {
+            var company = _dbContext.Companies.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (company != null)
+            {
+                throw new Exception("The company already exists");
+            }
+            var officeAddress = _insideEntityService.GetAddressById(command.OfficeAddressId);
+            var founder = _insideEntityService.GetCitizenById(command.FounderId);
+
+            var entity = new Company
+            {
+                CIF = command.CIF,
+                Name = command.Name,
+                FoundationYear = command.FoundationYear,
+                Domain = command.Domain,
+                Description = command.Description,
+                Status = command.Status,
+                DeletionDate = command.DeletionDate,
+                Founder = founder,
+                OfficeAddress = officeAddress
+            };
+
+            _dbContext.Companies.Add(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public async Task<RequestResponse> DeleteCompany(DeleteCompanyCommand command, CancellationToken cancellationToken)
+        {
+            var company = _dbContext.Companies.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (company == null)
+            {
+                throw new Exception("The company does not exists");
+            }
+
+            _dbContext.Companies.Remove(company);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public CompanyResponse GetCompanyById(GetCompanyByIdQuery query)
+        {
+            var result = _dbContext.Companies
+                .Where(v => v.Identifier == query.Identifier)
+                .ProjectTo<CompanyResponse>(_mapper.ConfigurationProvider)
+                .FirstOrDefault();
+            return result;
+        }
+
+        public List<CompanyResponse> GetCompanies(GetCompaniesQuery query)
+        {
+            var result = _dbContext.Companies
+                .ProjectTo<CompanyResponse>(_mapper.ConfigurationProvider)
+                .ToList();
+            return result;
+        }
+
+        public async Task<RequestResponse> UpdateCompany(UpdateCompanyCommand command, CancellationToken cancellationToken)
+        {
+            var company = _dbContext.Companies.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (company == null)
+            {
+                throw new Exception("The company does not exists");
+            }
+            var officeAddress = _insideEntityService.GetAddressById(command.OfficeAddressId);
+            var founder = _insideEntityService.GetCitizenById(command.FounderId);
+
+            company.CIF = command.CIF;
+            company.Name = command.Name;
+            company.FoundationYear = command.FoundationYear;
+            company.Domain = command.Domain;
+            company.Description = command.Description;
+            company.Status = command.Status;
+            company.DeletionDate = command.DeletionDate;
+            company.Founder = founder;
+            company.OfficeAddress = officeAddress;
+
+            _dbContext.Companies.Update(company);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+    }
+}
