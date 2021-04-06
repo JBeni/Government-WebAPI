0b5789bbef113a1dd481bf44830e3c17a7da2668
JBeni <jitca.beniamin@gmail.com>
2021-04-06 17:45:43 +0300

    create controller for PolicePayment

---
+++ WebUI/Controllers/PolicePaymentsController.cs
@@ -0,0 +1,45 @@
+﻿using GovernmentSystem.Application.Handlers.PolicePayments.Commands;
+using GovernmentSystem.Application.Handlers.PolicePayments.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.WebUI.Controllers
+{
+    public class PolicePaymentsController : ApiControllerBase
+    {
+        [HttpGet("getPolicePaymentById")]
+        public async Task<IActionResult> GetPolicePaymentById([FromQuery] GetPolicePaymentByIdQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("getPolicePayments")]
+        public async Task<IActionResult> GetPolicePayments([FromQuery] GetPolicePaymentsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CreatePolicePaymentCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(UpdatePolicePaymentCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(DeletePolicePaymentCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+    }
+}
