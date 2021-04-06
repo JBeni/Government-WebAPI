4351c6c1e370d404b80797b61976a4f580124f9c
JBeni <jitca.beniamin@gmail.com>
2021-04-06 13:04:20 +0300

    create Payment model for payments entities

---
+++ Domain/Common/Payment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GovernmentSystem.Domain.Common
+{
+    public class Payment : AuditEntity
+    {
+        public string Title { get; set; }
+        public long AmountPaid { get; set; }
+        public long AmountToPay { get; set; }
+        public DateTime DateOfPayment { get; set; }
+    }
+}
