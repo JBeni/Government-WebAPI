437cb6dbf69011c0cbf9c6bab271f31a4c345049
JBeni <jitca.beniamin@gmail.com>
2021-04-06 23:24:05 +0300

    create FraudOfficeReportProblem configuration

---
+++ Infrastructure/Persistence/Configurations/FraudOfficeReportProblemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using GovernmentSystem.Domain.Entities.SeriousFraudOffices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class FraudOfficeReportProblemConfiguration : IEntityTypeConfiguration<FraudOfficeReportProblem>
+    {
+        public void Configure(EntityTypeBuilder<FraudOfficeReportProblem> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.Title)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Description)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.IsProcessed)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
