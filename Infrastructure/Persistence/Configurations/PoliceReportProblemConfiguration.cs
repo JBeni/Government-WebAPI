8d258686ffb60d070f42a3a3049a4e47ac7efd31
JBeni <jitca.beniamin@gmail.com>
2021-04-06 18:23:19 +0300

    create PoliceReportProblem configuration

---
+++ Infrastructure/Persistence/Configurations/PoliceReportProblemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using GovernmentSystem.Domain.Entities.PoliceStations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class PoliceReportProblemConfiguration : IEntityTypeConfiguration<PoliceReportProblem>
+    {
+        public void Configure(EntityTypeBuilder<PoliceReportProblem> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.Title)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Description)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.IsProcessed)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
