a349d451c5c54ff1a5dc0350c9e4a1a78773ad29
JBeni <jitca.beniamin@gmail.com>
2021-04-06 18:23:41 +0300

    create PoliceReportProblem controller

---
+++ WebUI/Controllers/PoliceReportProblemController.cs
@@ -0,0 +1,45 @@
+﻿using GovernmentSystem.Application.Handlers.PoliceReportProblems.Commands;
+using GovernmentSystem.Application.Handlers.PoliceReportProblems.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.WebUI.Controllers
+{
+    public class PoliceReportProblemsController : ApiControllerBase
+    {
+        [HttpGet("getPoliceReportProblemById")]
+        public async Task<IActionResult> GetPoliceReportProblemById([FromQuery] GetPoliceReportProblemByIdQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("getPoliceReportProblems")]
+        public async Task<IActionResult> GetPoliceReportProblems([FromQuery] GetPoliceReportProblemsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CreatePoliceReportProblemCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(UpdatePoliceReportProblemCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(DeletePoliceReportProblemCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+    }
+}
