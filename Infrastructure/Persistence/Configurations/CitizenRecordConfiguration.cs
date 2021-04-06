1ca701c12a2a548b4b4e70f98d84f24295e27dd1
JBeni <jitca.beniamin@gmail.com>
2021-04-06 15:23:17 +0300

    create CitizenRecord configuration

---
+++ Infrastructure/Persistence/Configurations/CitizenRecordConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using GovernmentSystem.Domain.Entities.PoliceStations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class CitizenRecordConfiguration : IEntityTypeConfiguration<CitizenRecord>
+    {
+        public void Configure(EntityTypeBuilder<CitizenRecord> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.Reason)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Description)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.CriminalityType)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.DateOfIssue)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Witnesses)
+                .HasMaxLength(550)
+                .IsRequired();
+        }
+    }
+}
