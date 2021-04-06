77a7a2f454d634bbf88848413129323b9b7a7420
JBeni <jitca.beniamin@gmail.com>
2021-04-06 23:01:54 +0300

    create Tax service

---
+++ Application/Interfaces/ITaxService.cs
@@ -0,0 +1,19 @@
+﻿using GovernmentSystem.Application.Handlers.Taxes.Commands;
+using GovernmentSystem.Application.Common.Models;
+using System.Threading;
+using System.Threading.Tasks;
+using GovernmentSystem.Application.Handlers.Taxes.Queries;
+using System.Collections.Generic;
+using GovernmentSystem.Application.Responses;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface ITaxService
+    {
+        Task<RequestResponse> CreateTax(CreateTaxCommand command, CancellationToken cancellationToken);
+        Task<RequestResponse> DeleteTax(DeleteTaxCommand command, CancellationToken cancellationToken);
+        TaxResponse GetTaxById(GetTaxByIdQuery query);
+        List<TaxResponse> GetTaxes(GetTaxesQuery query);
+        Task<RequestResponse> UpdateTax(UpdateTaxCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ Infrastructure/Services/TaxService.cs
@@ -0,0 +1,108 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GovernmentSystem.Application.Common.Interfaces;
+using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.Taxes.Queries;
+using GovernmentSystem.Application.Handlers.Taxes.Commands;
+using GovernmentSystem.Application.Interfaces;
+using GovernmentSystem.Application.Responses;
+using GovernmentSystem.Domain.Entities.SeriousFraudOffices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Infrastructure.Services
+{
+    public class TaxService : ITaxService
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly IInsideEntityService _insideEntityService;
+
+        public TaxService(IApplicationDbContext dbContext, IMapper mapper, IInsideEntityService insideEntityService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _insideEntityService = insideEntityService;
+        }
+
+        public async Task<RequestResponse> CreateTax(CreateTaxCommand command, CancellationToken cancellationToken)
+        {
+            var Tax = _dbContext.Taxes.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (Tax != null)
+            {
+                throw new Exception("The tax already exists");
+            }
+            var company = _insideEntityService.GetCompanyById(command.CompanyId);
+            var citizen = _insideEntityService.GetCitizenById(command.CitizenId);
+
+            var entity = new Tax
+            {
+                Title = command.Title,
+                Description = command.Description,
+                AmountToPay = command.AmountToPay,
+                AmountPaid = command.AmountPaid,
+                Company = company,
+                Citizen = citizen,
+            };
+
+            _dbContext.Taxes.Add(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public async Task<RequestResponse> DeleteTax(DeleteTaxCommand command, CancellationToken cancellationToken)
+        {
+            var Tax = _dbContext.Taxes.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (Tax == null)
+            {
+                throw new Exception("The tax does not exists");
+            }
+
+            _dbContext.Taxes.Remove(Tax);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public TaxResponse GetTaxById(GetTaxByIdQuery query)
+        {
+            var result = _dbContext.Taxes
+                .Where(v => v.Identifier == query.Identifier)
+                .ProjectTo<TaxResponse>(_mapper.ConfigurationProvider)
+                .FirstOrDefault();
+            return result;
+        }
+
+        public List<TaxResponse> GetTaxes(GetTaxesQuery query)
+        {
+            var result = _dbContext.Taxes
+                .ProjectTo<TaxResponse>(_mapper.ConfigurationProvider)
+                .ToList();
+            return result;
+        }
+
+        public async Task<RequestResponse> UpdateTax(UpdateTaxCommand command, CancellationToken cancellationToken)
+        {
+            var Tax = _dbContext.Taxes.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (Tax == null)
+            {
+                throw new Exception("The tax does not exists");
+            }
+            var company = _insideEntityService.GetCompanyById(command.CompanyId);
+            var citizen = _insideEntityService.GetCitizenById(command.CitizenId);
+
+            Tax.Title = command.Title;
+            Tax.Description = command.Description;
+            Tax.AmountToPay = command.AmountToPay;
+            Tax.AmountPaid = command.AmountPaid;
+            Tax.Company = company;
+            Tax.Citizen = citizen;
+
+            _dbContext.Taxes.Update(Tax);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+    }
+}
