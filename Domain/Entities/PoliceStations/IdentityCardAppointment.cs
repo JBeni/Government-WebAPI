abc7bd3e03c9f1d9a1fbdd9c93b3b7fbc8a13a91
JBeni <jitca.beniamin@gmail.com>
2021-04-06 13:05:27 +0300

    create IdentityCardAppointment entity

--- Domain/Entities/PoliceStations/IdentityCardAppointment.cs
+++ Domain/Entities/PoliceStations/IdentityCardAppointment.cs
@@ -1,8 +1,14 @@
 ﻿using GovernmentSystem.Domain.Common;
+using GovernmentSystem.Domain.Entities.Citizens;
+using System;
 
 namespace GovernmentSystem.Domain.Entities.PoliceStations
 {
     public class IdentityCardAppointment : AuditEntity
     {
+        public string Reason { get; set; }
+        public DateTime AppointmentDay { get; set; }
+        public string AdditionalInformation { get; set; }
+        public Citizen Citizen { get; set; }
     }
 }
