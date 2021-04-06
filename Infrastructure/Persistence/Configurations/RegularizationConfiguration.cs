896349fbb630f3646a3a14dc6ef1d2075f25b33f
JBeni <jitca.beniamin@gmail.com>
2021-04-06 21:36:40 +0300

    create Regularization configuration

---
+++ Infrastructure/Persistence/Configurations/RegularizationConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using GovernmentSystem.Domain.Entities.SeriousFraudOffices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class RegularizationConfiguration : IEntityTypeConfiguration<Regularization>
+    {
+        public void Configure(EntityTypeBuilder<Regularization> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.Informations)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.LawName)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.ModificationRequired)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.CompanyImpact)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
