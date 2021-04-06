88af53b1ad740bed91c4ea38bdf79453ce2c7f33
JBeni <jitca.beniamin@gmail.com>
2021-04-06 10:47:36 +0300

    added members for Regularization Entity

--- Domain/Entities/SeriousFraudOffices/Regularization.cs
+++ Domain/Entities/SeriousFraudOffices/Regularization.cs
@@ -4,5 +4,9 @@ namespace GovernmentSystem.Domain.Entities.SeriousFraudOffices
 {
     public class Regularization : AuditEntity
     {
+        public string Informations { get; set; }
+        public string LawName { get; set; }
+        public string ModificationRequired { get; set; }
+        public string CompanyImpact { get; set; }
     }
 }
