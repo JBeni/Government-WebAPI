a1cf89f9bb056db207556c8bf07df05f28d65b34
JBeni <jitca.beniamin@gmail.com>
2021-04-06 22:21:52 +0300

    create Company configuration

---
+++ Infrastructure/Persistence/Configurations/CompanyConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using GovernmentSystem.Domain.Entities.SeriousFraudOffices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class CompanyConfiguration : IEntityTypeConfiguration<Company>
+    {
+        public void Configure(EntityTypeBuilder<Company> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.CIF)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Name)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.FoundationYear)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Domain)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Description)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Status)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.DeletionDate)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
