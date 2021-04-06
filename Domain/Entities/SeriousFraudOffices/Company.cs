5b67383545d7402f1afac2642f438d1c7abd1adc
JBeni <jitca.beniamin@gmail.com>
2021-04-06 10:47:23 +0300

    added members for Company Entity

--- Domain/Entities/SeriousFraudOffices/Company.cs
+++ Domain/Entities/SeriousFraudOffices/Company.cs
@@ -1,8 +1,20 @@
 ﻿using GovernmentSystem.Domain.Common;
+using GovernmentSystem.Domain.Entities.Citizens;
+using GovernmentSystem.Domain.Entities.CityHalls;
+using System;
 
 namespace GovernmentSystem.Domain.Entities.SeriousFraudOffices
 {
     public class Company : AuditEntity
     {
+        public string CIF { get; set; }
+        public string Name { get; set; }
+        public DateTime FoundationYear { get; set; }
+        public string Domain { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public DateTime DeletionDate { get; set; }
+        public Citizen Founder { get; set; }
+        public Address OfficeAddress { get; set; }
     }
 }
