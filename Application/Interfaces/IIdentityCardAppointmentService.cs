1f3e1bae81a61c0a0b5962480800d1033c31692f
JBeni <jitca.beniamin@gmail.com>
2021-04-06 16:01:35 +0300

    create IdentityCardAppointment service

---
+++ Application/Interfaces/IIdentityCardAppointmentService.cs
@@ -0,0 +1,19 @@
+﻿using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.IdentityCardAppointments.Commands;
+using GovernmentSystem.Application.Handlers.IdentityCardAppointments.Queries;
+using GovernmentSystem.Application.Responses;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface IIdentityCardAppointmentService
+    {
+        Task<RequestResponse> CreateIdentityCardAppointment(CreateIdentityCardAppointmentCommand command, CancellationToken cancellationToken);
+        Task<RequestResponse> DeleteIdentityCardAppointment(DeleteIdentityCardAppointmentCommand command, CancellationToken cancellationToken);
+        IdentityCardAppointmentResponse GetIdentityCardAppointmentById(GetIdentityCardAppointmentByIdQuery query);
+        List<IdentityCardAppointmentResponse> GetIdentityCardAppointments(GetIdentityCardAppointmentsQuery query);
+        Task<RequestResponse> UpdateIdentityCardAppointment(UpdateIdentityCardAppointmentCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ Application/Responses/IdentityCardAppointmentResponse.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using GovernmentSystem.Application.Common.Mappings;
+using GovernmentSystem.Domain.Entities.Citizens;
+using GovernmentSystem.Domain.Entities.PoliceStations;
+using System;
+
+namespace GovernmentSystem.Application.Responses
+{
+    public class IdentityCardAppointmentResponse : IMapFrom<IdentityCardAppointment>
+    {
+        public Guid Identifier { get; set; }
+        public string Reason { get; set; }
+        public DateTime AppointmentDay { get; set; }
+        public string AdditionalInformation { get; set; }
+        public Citizen Citizen { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<IdentityCardAppointment, IdentityCardAppointmentResponse>()
+                .ForMember(d => d.Identifier, opt => opt.MapFrom(s => s.Identifier))
+                .ForMember(d => d.Reason, opt => opt.MapFrom(s => s.Reason))
+                .ForMember(d => d.AppointmentDay, opt => opt.MapFrom(s => s.AppointmentDay))
+                .ForMember(d => d.AdditionalInformation, opt => opt.MapFrom(s => s.AdditionalInformation))
+                .ForMember(d => d.Citizen, opt => opt.MapFrom(s => s.Citizen));
+        }
+    }
+}
