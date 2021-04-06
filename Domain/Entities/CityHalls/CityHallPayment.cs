a0bc1d05266683aa441a79fffafa42584fddbfd0
JBeni <jitca.beniamin@gmail.com>
2021-04-06 13:04:33 +0300

    create CityHallPayment entity

---
+++ Domain/Entities/CityHalls/CityHallPayment.cs
@@ -0,0 +1,12 @@
+﻿using GovernmentSystem.Domain.Common;
+using GovernmentSystem.Domain.Entities.Citizens;
+
+namespace GovernmentSystem.Domain.Entities.CityHalls
+{
+    public class CityHallPayment : Payment
+    {
+        public Invoice Invoice { get; set; }
+        public CityHall CityHall { get; set; }
+        public Citizen Citizen { get; set; }
+    }
+}
