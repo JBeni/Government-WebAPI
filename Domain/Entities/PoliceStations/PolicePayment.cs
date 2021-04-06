e542a6706cf92c78bfc02ba4281c59a64330ea57
JBeni <jitca.beniamin@gmail.com>
2021-04-06 13:05:37 +0300

    create PolicePayment entity

--- Domain/Entities/PoliceStations/PolicePayment.cs
+++ Domain/Entities/PoliceStations/PolicePayment.cs
@@ -1,8 +1,12 @@
 ﻿using GovernmentSystem.Domain.Common;
+using GovernmentSystem.Domain.Entities.Citizens;
 
 namespace GovernmentSystem.Domain.Entities.PoliceStations
 {
-    public class PolicePayment : AuditEntity
+    public class PolicePayment : Payment
     {
+        public Invoice Invoice { get; set; }
+        public PoliceStation PoliceStation { get; set; }
+        public Citizen Citizen { get; set; }
     }
 }
