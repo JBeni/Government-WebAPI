5d4a0c08505abbb90f74c99668f427575482e21b
JBeni <jitca.beniamin@gmail.com>
2021-04-06 13:05:13 +0300

    create CitizenRecord entity

--- Domain/Entities/PoliceStations/CitizenRecord.cs
+++ Domain/Entities/PoliceStations/CitizenRecord.cs
@@ -1,8 +1,17 @@
 ﻿using GovernmentSystem.Domain.Common;
+using GovernmentSystem.Domain.Entities.Citizens;
+using System;
 
 namespace GovernmentSystem.Domain.Entities.PoliceStations
 {
     public class CitizenRecord : AuditEntity
     {
+        public string Reason { get; set; }
+        public string Description { get; set; }
+        public string CriminalityType { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public string Witnesses { get; set; }
+        public PoliceStation PoliceStation { get; set; }
+        public Citizen Citizen { get; set; }
     }
 }
