8c2880f6ccd8423f367e0cc66102fb66dcb79089
JBeni <jitca.beniamin@gmail.com>
2021-04-06 13:04:42 +0300

    create MedicalPayment entity

--- Domain/Entities/Medicals/MedicalPayment.cs
+++ Domain/Entities/Medicals/MedicalPayment.cs
@@ -1,15 +1,11 @@
 ﻿using GovernmentSystem.Domain.Common;
 using GovernmentSystem.Domain.Entities.Citizens;
-using System;
 
 namespace GovernmentSystem.Domain.Entities.Medicals
 {
-    public class MedicalPayment : AuditEntity
+    public class MedicalPayment : Payment
     {
-        public long AmountPaid { get; set; }
-        public long AmountToPay { get; set; }
-        public DateTime DateOfPayment { get; set; }
-
+        public Invoice Invoice { get; set; }
         public MedicalProcedure MedicalProcedure { get; set; }
         public MedicalCenter MedicalCenter { get; set; }
         public PublicServantMedicalCenter PublicServantMedicalCenter { get; set; }
