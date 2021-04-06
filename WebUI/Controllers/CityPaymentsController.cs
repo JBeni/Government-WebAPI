88563ecb187b988fff3b255c7f9426ad671b1726
JBeni <jitca.beniamin@gmail.com>
2021-04-06 18:54:32 +0300

    create controller CityPayment

---
+++ WebUI/Controllers/CityPaymentsController.cs
@@ -0,0 +1,45 @@
+﻿using GovernmentSystem.Application.Handlers.CityPayments.Commands;
+using GovernmentSystem.Application.Handlers.CityPayments.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.WebUI.Controllers
+{
+    public class CityPaymentsController : ApiControllerBase
+    {
+        [HttpGet("getCityPaymentById")]
+        public async Task<IActionResult> GetCityPaymentById([FromQuery] GetCityPaymentByIdQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("getCityPayments")]
+        public async Task<IActionResult> GetCityPayments([FromQuery] GetCityPaymentsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CreateCityPaymentCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(UpdateCityPaymentCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(DeleteCityPaymentCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+    }
+}
