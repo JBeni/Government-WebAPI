7f3abd81e05373d69db2605d26132620a0a9e0a9
JBeni <jitca.beniamin@gmail.com>
2021-04-06 21:36:57 +0300

    create Regularization controller

---
+++ WebUI/Controllers/RegularizationsController.cs
@@ -0,0 +1,45 @@
+﻿using GovernmentSystem.Application.Handlers.Regularizations.Commands;
+using GovernmentSystem.Application.Handlers.Regularizations.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.WebUI.Controllers
+{
+    public class RegularizationsController : ApiControllerBase
+    {
+        [HttpGet("getRegularizationById")]
+        public async Task<IActionResult> GetRegularizationById([FromQuery] GetRegularizationByIdQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("getRegularizations")]
+        public async Task<IActionResult> GetRegularizations([FromQuery] GetRegularizationsQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CreateRegularizationCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(UpdateRegularizationCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(DeleteRegularizationCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+    }
+}
