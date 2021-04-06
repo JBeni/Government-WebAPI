f9a5c392db73f3f7458ea4cc1b1156582835af6c
JBeni <jitca.beniamin@gmail.com>
2021-04-06 18:53:41 +0300

    create CityPayment service

---
+++ Application/Interfaces/ICityPaymentService.cs
@@ -0,0 +1,19 @@
+﻿using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.CityPayments.Commands;
+using GovernmentSystem.Application.Handlers.CityPayments.Queries;
+using GovernmentSystem.Application.Responses;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface ICityPaymentService
+    {
+        Task<RequestResponse> CreateCityPayment(CreateCityPaymentCommand command, CancellationToken cancellationToken);
+        Task<RequestResponse> DeleteCityPayment(DeleteCityPaymentCommand command, CancellationToken cancellationToken);
+        CityPaymentResponse GetCityPaymentById(GetCityPaymentByIdQuery query);
+        List<CityPaymentResponse> GetCityPayments(GetCityPaymentsQuery query);
+        Task<RequestResponse> UpdateCityPayment(UpdateCityPaymentCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ Infrastructure/Services/CityPaymentService.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GovernmentSystem.Application.Common.Interfaces;
+using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.CityPayments.Commands;
+using GovernmentSystem.Application.Handlers.CityPayments.Queries;
+using GovernmentSystem.Application.Interfaces;
+using GovernmentSystem.Application.Responses;
+using GovernmentSystem.Domain.Entities.CityHalls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Infrastructure.Services
+{
+    public class CityPaymentService : ICityPaymentService
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly IInsideEntityService _insideEntityService;
+
+        public CityPaymentService(IApplicationDbContext dbContext, IMapper mapper, IInsideEntityService insideEntityService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _insideEntityService = insideEntityService;
+        }
+
+        public async Task<RequestResponse> CreateCityPayment(CreateCityPaymentCommand command, CancellationToken cancellationToken)
+        {
+            var cityPayment = _dbContext.CityPayments.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (cityPayment != null)
+            {
+                throw new Exception("The city payment already exists");
+            }
+            var citizen = _insideEntityService.GetCitizenById(command.CitizenId);
+            var cityHall = _insideEntityService.GetCityHallById(command.CityHallId);
+            var invoice = _insideEntityService.GetInvoiceById(command.InvoiceId);
+
+            var entity = new CityPayment
+            {
+                Title = command.Title,
+                AmountPaid = command.AmountPaid,
+                AmountToPay = command.AmountToPay,
+                DateOfPayment = command.DateOfPayment,
+                Citizen = citizen,
+                CityHall = cityHall,
+                Invoice = invoice,
+            };
+
+            _dbContext.CityPayments.Add(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public async Task<RequestResponse> DeleteCityPayment(DeleteCityPaymentCommand command, CancellationToken cancellationToken)
+        {
+            var cityPayment = _dbContext.CityPayments.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (cityPayment != null)
+            {
+                throw new Exception("The city payment does not exists");
+            }
+
+            _dbContext.CityPayments.Remove(cityPayment);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public CityPaymentResponse GetCityPaymentById(GetCityPaymentByIdQuery query)
+        {
+            var result = _dbContext.CityPayments
+                .Where(x => x.Identifier == query.Identifier)
+                .ProjectTo<CityPaymentResponse>(_mapper.ConfigurationProvider)
+                .FirstOrDefault();
+            return result;
+        }
+
+        public List<CityPaymentResponse> GetCityPayments(GetCityPaymentsQuery query)
+        {
+            var result = _dbContext.CityPayments
+                .ProjectTo<CityPaymentResponse>(_mapper.ConfigurationProvider)
+                .ToList();
+            return result;
+        }
+
+        public async Task<RequestResponse> UpdateCityPayment(UpdateCityPaymentCommand command, CancellationToken cancellationToken)
+        {
+            var cityPayment = _dbContext.CityPayments.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (cityPayment != null)
+            {
+                throw new Exception("The city payment does not exists");
+            }
+            var citizen = _insideEntityService.GetCitizenById(command.CitizenId);
+            var cityHall = _insideEntityService.GetCityHallById(command.CityHallId);
+            var invoice = _insideEntityService.GetInvoiceById(command.InvoiceId);
+
+            cityPayment.Title = command.Title;
+            cityPayment.AmountPaid = command.AmountPaid;
+            cityPayment.AmountToPay = command.AmountToPay;
+            cityPayment.DateOfPayment = command.DateOfPayment;
+            cityPayment.Citizen = citizen;
+            cityPayment.CityHall = cityHall;
+            cityPayment.Invoice = invoice;
+
+            _dbContext.CityPayments.Update(cityPayment);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+    }
+}
