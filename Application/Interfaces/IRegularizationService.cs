e5b5409b0b434552a89fa763022b4bef90b79d02
JBeni <jitca.beniamin@gmail.com>
2021-04-06 21:36:27 +0300

    create Regularization service

---
+++ Application/Interfaces/IRegularizationService.cs
@@ -0,0 +1,19 @@
+﻿using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.Regularizations.Commands;
+using GovernmentSystem.Application.Handlers.Regularizations.Queries;
+using GovernmentSystem.Application.Responses;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface IRegularizationService
+    {
+        Task<RequestResponse> CreateRegularization(CreateRegularizationCommand command, CancellationToken cancellationToken);
+        Task<RequestResponse> DeleteRegularization(DeleteRegularizationCommand command, CancellationToken cancellationToken);
+        RegularizationResponse GetRegularizationById(GetRegularizationByIdQuery query);
+        List<RegularizationResponse> GetRegularizations(GetRegularizationsQuery query);
+        Task<RequestResponse> UpdateRegularization(UpdateRegularizationCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ Infrastructure/Services/RegularizationService.cs
@@ -0,0 +1,99 @@
+﻿using GovernmentSystem.Application.Interfaces;
+using GovernmentSystem.Application.Common.Interfaces;
+using GovernmentSystem.Application.Common.Models;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using GovernmentSystem.Application.Responses;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GovernmentSystem.Domain.Entities.SeriousFraudOffices;
+using GovernmentSystem.Application.Handlers.Regularizations.Commands;
+using GovernmentSystem.Application.Handlers.Regularizations.Queries;
+
+namespace GovernmentSystem.Infrastructure.Services
+{
+    public class RegularizationService : IRegularizationService
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly IInsideEntityService _insideEntityService;
+
+        public RegularizationService(IApplicationDbContext dbContext, IMapper mapper, IInsideEntityService insideEntityService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _insideEntityService = insideEntityService;
+        }
+
+        public async Task<RequestResponse> CreateRegularization(CreateRegularizationCommand command, CancellationToken cancellationToken)
+        {
+            var regularization = _dbContext.Regularizations.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (regularization != null)
+            {
+                throw new Exception("The regularization already exists");
+            }
+            var entity = new Regularization
+            {
+                CompanyImpact = command.CompanyImpact,
+                Informations = command.Informations,
+                LawName = command.LawName,
+                ModificationRequired = command.ModificationRequired
+            };
+
+            _dbContext.Regularizations.Add(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public async Task<RequestResponse> DeleteRegularization(DeleteRegularizationCommand command, CancellationToken cancellationToken)
+        {
+            var regularization = _dbContext.Regularizations.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (regularization == null)
+            {
+                throw new Exception("The regularization does not exists");
+            }
+
+            _dbContext.Regularizations.Remove(regularization);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public List<RegularizationResponse> GetRegularizations(GetRegularizationsQuery query)
+        {
+            var result = _dbContext.Regularizations
+                .ProjectTo<RegularizationResponse>(_mapper.ConfigurationProvider)
+                .ToList();
+            return result;
+        }
+
+        public RegularizationResponse GetRegularizationById(GetRegularizationByIdQuery query)
+        {
+            var result = _dbContext.Regularizations
+                .Where(x => x.Identifier == query.Identifier)
+                .ProjectTo<RegularizationResponse>(_mapper.ConfigurationProvider)
+                .FirstOrDefault();
+            return result;
+        }
+
+        public async Task<RequestResponse> UpdateRegularization(UpdateRegularizationCommand command, CancellationToken cancellationToken)
+        {
+            var regularization = _dbContext.Regularizations.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (regularization == null)
+            {
+                throw new Exception("The regularization does not exists");
+            }
+            regularization.CompanyImpact = command.CompanyImpact;
+            regularization.Informations = command.Informations;
+            regularization.LawName = command.LawName;
+            regularization.ModificationRequired = command.ModificationRequired;
+
+
+            _dbContext.Regularizations.Update(regularization);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+    }
+}
