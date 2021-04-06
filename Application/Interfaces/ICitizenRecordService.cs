624f5b4ccb1d883dd4bdcc4c189227dba1b87d2a
JBeni <jitca.beniamin@gmail.com>
2021-04-06 14:58:16 +0300

    create CitizenRecord service

---
+++ Application/Interfaces/ICitizenRecordService.cs
@@ -0,0 +1,19 @@
+﻿using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.CitizenRecords.Commands;
+using GovernmentSystem.Application.Handlers.CitizenRecords.Queries;
+using GovernmentSystem.Application.Responses;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface ICitizenRecordService
+    {
+        Task<RequestResponse> CreateCitizenRecord(CreateCitizenRecordCommand command, CancellationToken cancellationToken);
+        Task<RequestResponse> DeleteCitizenRecord(DeleteCitizenRecordCommand command, CancellationToken cancellationToken);
+        CitizenRecordResponse GetCitizenRecordById(GetCitizenRecordByIdQuery query);
+        List<CitizenRecordResponse> GetCitizenRecords(GetCitizenRecordsQuery query);
+        Task<RequestResponse> UpdateCitizenRecord(UpdateCitizenRecordCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ Application/Responses/CitizenRecordResponse.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using GovernmentSystem.Application.Common.Mappings;
+using GovernmentSystem.Domain.Entities.Citizens;
+using GovernmentSystem.Domain.Entities.PoliceStations;
+using System;
+
+namespace GovernmentSystem.Application.Responses
+{
+    public class CitizenRecordResponse : IMapFrom<CitizenRecord>
+    {
+        public Guid Identifier { get; set; }
+        public string Reason { get; set; }
+        public string Description { get; set; }
+        public string CriminalityType { get; set; }
+        public DateTime DateOfIssue { get; set; }
+        public string Witnesses { get; set; }
+        public PoliceStation PoliceStation { get; set; }
+        public Citizen Citizen { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<CitizenRecord, CitizenRecordResponse>()
+                .ForMember(d => d.Identifier, opt => opt.MapFrom(s => s.Identifier))
+                .ForMember(d => d.Reason, opt => opt.MapFrom(s => s.Reason))
+                .ForMember(d => d.Description, opt => opt.MapFrom(s => s.Description))
+                .ForMember(d => d.CriminalityType, opt => opt.MapFrom(s => s.CriminalityType))
+                .ForMember(d => d.DateOfIssue, opt => opt.MapFrom(s => s.DateOfIssue))
+                .ForMember(d => d.Witnesses, opt => opt.MapFrom(s => s.Witnesses))
+                .ForMember(d => d.PoliceStation, opt => opt.MapFrom(s => s.PoliceStation))
+                .ForMember(d => d.Citizen, opt => opt.MapFrom(s => s.Citizen));
+        }
+    }
+}
