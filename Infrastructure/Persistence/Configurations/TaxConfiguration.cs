4bca8061b6f0e6d8476d1eca991064cbf55c8585
JBeni <jitca.beniamin@gmail.com>
2021-04-06 23:02:04 +0300

    create Tax configuration

---
+++ Infrastructure/Persistence/Configurations/TaxConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using GovernmentSystem.Domain.Entities.SeriousFraudOffices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class TaxConfiguration : IEntityTypeConfiguration<Tax>
+    {
+        public void Configure(EntityTypeBuilder<Tax> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.Title)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Description)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.AmountToPay)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.AmountPaid)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
