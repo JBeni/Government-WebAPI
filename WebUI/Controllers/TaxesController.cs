5d95e8b716d45bcea0c92bc8ca2b09e065e4a755
JBeni <jitca.beniamin@gmail.com>
2021-04-06 23:02:25 +0300

    create Tax controller

---
+++ WebUI/Controllers/TaxesController.cs
@@ -0,0 +1,45 @@
+﻿using GovernmentSystem.Application.Handlers.Taxes.Commands;
+using GovernmentSystem.Application.Handlers.Taxes.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.WebUI.Controllers
+{
+    public class TaxesController : ApiControllerBase
+    {
+        [HttpGet("getTaxById")]
+        public async Task<IActionResult> GetTaxById([FromQuery] GetTaxByIdQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("getTaxes")]
+        public async Task<IActionResult> GetTaxes([FromQuery] GetTaxesQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CreateTaxCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(UpdateTaxCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(DeleteTaxCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+    }
+}
