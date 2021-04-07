f80f88cafa5c67e5bc03a55698cfe9fddb703dab
JBeni <jitca.beniamin@gmail.com>
2021-04-07 12:04:09 +0300

    create PaymentHistory service

---
+++ Application/Interfaces/IPaymentHistoryService.cs
@@ -0,0 +1,19 @@
+﻿using GovernmentSystem.Application.Handlers.PaymentHistories.Commands;
+using GovernmentSystem.Application.Common.Models;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using GovernmentSystem.Application.Handlers.PaymentHistories.Queries;
+using GovernmentSystem.Application.Responses;
+
+namespace GovernmentSystem.Application.Interfaces
+{
+    public interface IPaymentHistoryService
+    {
+        Task<RequestResponse> CreatePaymentHistory(CreatePaymentHistoryCommand command, CancellationToken cancellationToken);
+        Task<RequestResponse> DeletePaymentHistory(DeletePaymentHistoryCommand command, CancellationToken cancellationToken);
+        PaymentHistoryResponse GetPaymentHistoryById(GetPaymentHistoryByIdQuery query);
+        List<PaymentHistoryResponse> GetPaymentHistories(GetPaymentHistoriesQuery query);
+        Task<RequestResponse> UpdatePaymentHistory(UpdatePaymentHistoryCommand command, CancellationToken cancellationToken);
+    }
+}
---
+++ Infrastructure/Services/PaymentHistoryService.cs
@@ -0,0 +1,107 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using GovernmentSystem.Application.Common.Interfaces;
+using GovernmentSystem.Application.Common.Models;
+using GovernmentSystem.Application.Handlers.PaymentHistories.Commands;
+using GovernmentSystem.Application.Handlers.PaymentHistories.Queries;
+using GovernmentSystem.Application.Interfaces;
+using GovernmentSystem.Application.Responses;
+using GovernmentSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.Infrastructure.Services
+{
+    public class PaymentHistoryService : IPaymentHistoryService
+    {
+        private readonly IApplicationDbContext _dbContext;
+        private readonly IMapper _mapper;
+        private readonly IInsideEntityService _insideEntityService;
+
+        public PaymentHistoryService(IApplicationDbContext dbContext, IMapper mapper, IInsideEntityService insideEntityService)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+            _insideEntityService = insideEntityService;
+        }
+
+        public async Task<RequestResponse> CreatePaymentHistory(CreatePaymentHistoryCommand command, CancellationToken cancellationToken)
+        {
+            var paymentHistory = _dbContext.PaymentHistories.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (paymentHistory != null)
+            {
+                throw new Exception("The payment history already exists");
+            }
+            var citizen = _insideEntityService.GetCitizenById(command.CitizenWhoPaidId);
+            var entity = new PaymentHistory
+            {
+                Title = command.Title,
+                Description = command.Description,
+                InstitutionType = command.InstitutionType,
+                AmountToPay = command.AmountToPay,
+                AmountPaid = command.AmountPaid,
+                DateOfPayment = command.DateOfPayment,
+                CitizenWhoPaid = citizen,
+            };
+
+            _dbContext.PaymentHistories.Add(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public async Task<RequestResponse> DeletePaymentHistory(DeletePaymentHistoryCommand command, CancellationToken cancellationToken)
+        {
+            var paymentHistory = _dbContext.PaymentHistories.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (paymentHistory == null)
+            {
+                throw new Exception("The payment history does not exists");
+            }
+
+            _dbContext.PaymentHistories.Remove(paymentHistory);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+
+        public PaymentHistoryResponse GetPaymentHistoryById(GetPaymentHistoryByIdQuery query)
+        {
+            var result = _dbContext.PaymentHistories
+                .Where(v => v.Identifier == query.Identifier)
+                .ProjectTo<PaymentHistoryResponse>(_mapper.ConfigurationProvider)
+                .FirstOrDefault();
+            return result;
+        }
+
+        public List<PaymentHistoryResponse> GetPaymentHistories(GetPaymentHistoriesQuery query)
+        {
+            var result = _dbContext.PaymentHistories
+                .ProjectTo<PaymentHistoryResponse>(_mapper.ConfigurationProvider)
+                .ToList();
+            return result;
+        }
+
+        public async Task<RequestResponse> UpdatePaymentHistory(UpdatePaymentHistoryCommand command, CancellationToken cancellationToken)
+        {
+            var paymentHistory = _dbContext.PaymentHistories.SingleOrDefault(x => x.Identifier == command.Identifier);
+            if (paymentHistory == null)
+            {
+                throw new Exception("The payment history does not exists");
+            }
+            var citizen = _insideEntityService.GetCitizenById(command.CitizenWhoPaidId);
+
+            paymentHistory.Title = command.Title;
+            paymentHistory.Description = command.Description;
+            paymentHistory.InstitutionType = command.InstitutionType;
+            paymentHistory.AmountToPay = command.AmountToPay;
+            paymentHistory.AmountPaid = command.AmountPaid;
+            paymentHistory.DateOfPayment = command.DateOfPayment;
+            paymentHistory.CitizenWhoPaid = citizen;
+
+            _dbContext.PaymentHistories.Update(paymentHistory);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return RequestResponse.Success();
+        }
+    }
+}
