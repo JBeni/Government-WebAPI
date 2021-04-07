664710953f70303f61d94ed6b53f4f715a590a4d
JBeni <jitca.beniamin@gmail.com>
2021-04-07 11:01:51 +0300

    create Invoice configuration

---
+++ Infrastructure/Persistence/Configurations/InvoiceConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using GovernmentSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class InvoiceConfiguration : IEntityTypeConfiguration<Invoice>
+    {
+        public void Configure(EntityTypeBuilder<Invoice> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.Title)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Description)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.InstitutionType)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.AmountToPay)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.IssuedDate)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.DueDate)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
