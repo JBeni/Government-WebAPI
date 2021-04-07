73ec2f3f3d07a4f935f373439c2641c670f0391b
JBeni <jitca.beniamin@gmail.com>
2021-04-07 09:24:01 +0300

    add members to Invoice Entity

--- Domain/Entities/Invoice.cs
+++ Domain/Entities/Invoice.cs
@@ -1,8 +1,15 @@
 ﻿using GovernmentSystem.Domain.Common;
+using System;
 
 namespace GovernmentSystem.Domain.Entities
 {
     public class Invoice : AuditEntity
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string InstitutionType { get; set; }
+        public long AmountToPay { get; set; }
+        public DateTime IssuedDate { get; set; }
+        public DateTime DueDate { get; set; }
     }
 }
