c75b36d7d9c054275531850c2e75d3f825f1d968
JBeni <jitca.beniamin@gmail.com>
2021-04-07 12:04:19 +0300

    create PaymentHistory configuration

---
+++ Infrastructure/Persistence/Configurations/PaymentHistoryConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using GovernmentSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GovernmentSystem.Infrastructure.Persistence.Configurations
+{
+    public class PaymentHistoryConfiguration : IEntityTypeConfiguration<PaymentHistory>
+    {
+        public void Configure(EntityTypeBuilder<PaymentHistory> builder)
+        {
+            builder.HasKey(x => x.Identifier);
+
+            builder.Property(t => t.Title)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.Description)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.InstitutionType)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.AmountToPay)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.AmountPaid)
+                .HasMaxLength(150)
+                .IsRequired();
+            builder.Property(t => t.DateOfPayment)
+                .HasMaxLength(150)
+                .IsRequired();
+        }
+    }
+}
