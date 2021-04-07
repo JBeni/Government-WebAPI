872656e5baab3cb8518e93c8f1936967cdd51ebb
JBeni <jitca.beniamin@gmail.com>
2021-04-07 09:24:11 +0300

    add members to PaymentHistory Entity

--- Domain/Entities/PaymentHistory.cs
+++ Domain/Entities/PaymentHistory.cs
@@ -1,8 +1,17 @@
 ﻿using GovernmentSystem.Domain.Common;
+using GovernmentSystem.Domain.Entities.Citizens;
+using System;
 
 namespace GovernmentSystem.Domain.Entities
 {
     public class PaymentHistory : AuditEntity
     {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string InstitutionType { get; set; }
+        public long AmountToPay { get; set; }
+        public long AmountPaid { get; set; }
+        public DateTime DateOfPayment { get; set; }
+        public Citizen CitizenWhoPaid { get; set; }
     }
 }
