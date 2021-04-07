92c002aaab6eb0d2f93f4d61aa2fb36c115ddf39
JBeni <jitca.beniamin@gmail.com>
2021-04-07 11:02:40 +0300

    create Invoice controller

---
+++ WebUI/Controllers/InvoicesController.cs
@@ -0,0 +1,45 @@
+﻿using GovernmentSystem.Application.Handlers.Invoices.Commands;
+using GovernmentSystem.Application.Handlers.Invoices.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace GovernmentSystem.WebUI.Controllers
+{
+    public class InvoicesController : ApiControllerBase
+    {
+        [HttpGet("getInvoiceById")]
+        public async Task<IActionResult> GetInvoiceById([FromQuery] GetInvoiceByIdQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpGet("getInvoices")]
+        public async Task<IActionResult> GetInvoices([FromQuery] GetInvoicesQuery query)
+        {
+            var result = await Mediator.Send(query);
+            return Ok(result);
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(CreateInvoiceCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpPut("update")]
+        public async Task<IActionResult> Update(UpdateInvoiceCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(DeleteInvoiceCommand command)
+        {
+            var result = await Mediator.Send(command);
+            return result.Successful == true ? Ok(result) : BadRequest(result.Exception.InnerException.Message ?? result.Exception.Message);
+        }
+    }
+}
