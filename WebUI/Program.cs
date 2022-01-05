b9ce66cb618367e4979fccfbe6845b569045eb27
JBeni <JBeni@users.noreply.github.com>
2022-01-05 11:55:14 +0200

    fix issues - upgrading from net 5 to 6 - program web api class

--- WebUI/Program.cs
+++ WebUI/Program.cs
@@ -1,50 +1,111 @@
-using GovernmentSystem.Infrastructure.Persistence;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
 
-namespace GovernmentSystem.WebUI
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
 {
-    public class Program
+    options.AddPolicy("EnableCORS", builder =>
     {
-        public async static Task Main(string[] args)
+        builder.AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddApplicationLayer();
+builder.Services.AddInfrastructureLayer(builder.Configuration);
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddSingleton<ICurrentUserService, CurrentUserService>();
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddHealthChecks().AddDbContextCheck<ApplicationDbContext>();
+
+builder.Services.AddControllers(options =>
+    options.Filters.Add<ApiExceptionFilterAttribute>()).AddFluentValidation();
+
+// Customise default API behaviour
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.AddApiVersioning(options =>
+{
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new ApiVersion(2, 0);
+    options.ReportApiVersions = true;
+
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new HeaderApiVersionReader("x-api-version"),
+        new MediaTypeApiVersionReader("x-api-version")
+    );
+
+    // ensure that only request with x-api-version=1 could access the methods of the controller
+    // MapToApiVersion("2.0") -> ensure the same behaviour but at method level
+    options.Conventions.Controller<AddressesController>()
+        .HasApiVersion(2, 0);
+    options.Conventions.Controller<AddressTypesController>()
+        .HasApiVersion(2, 0);
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v2", new OpenApiInfo { Title = "WebUI", Version = "v2" });
+});
+
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    try
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        if (context.Database.IsSqlServer())
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-
-                try
-                {
-                    var context = services.GetRequiredService<ApplicationDbContext>();
-                    if (context.Database.IsSqlServer())
-                    {
-                        context.Database.Migrate();
-                    }
-
-                    //await ApplicationDbContextSeed.SeedSampleDataAsync(context);
-                    //await ApplicationDbContextSeed.SeedSampleDriverLicenseCategoryAsync(context);
-                }
-                catch (Exception ex)
-                {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
-                    throw;
-                }
-            }
-            await host.RunAsync();
+            context.Database.Migrate();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        //await ApplicationDbContextSeed.SeedSampleDataAsync(context);
+        //await ApplicationDbContextSeed.SeedSampleDriverLicenseCategoryAsync(context);
+    }
+    catch (Exception ex)
+    {
+        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+        Log.Logger.Error(ex, "An error occurred while migrating or seeding the database.");
+        throw;
     }
 }
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v2/swagger.json", "WebUI v2"));
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseCors("EnableCORS");
+app.UseHealthChecks("/health");
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.Run();
